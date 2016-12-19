2a66de6dbd27de7af1b3874a34e6635d56d3d127
Macc Liu <maccliu@qq.com>
2016-12-20 00:55:02 +0800

    对Container-Interop规范的ContainerException和NotFoundException的实现

---
+++ src/Service/ContainerException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Soil: An underlying PHP framework, under the MIT license.
+ *
+ * Copyright (c) 2016-2017, Macc Liu <mail@maccliu.com>
+ * WITHOUT WARRANTY OF ANY KIND
+ */
+
+namespace Soil\Service;
+
+use Interop\Container\Exception\ContainerException;
+
+/**
+ * ContainerException
+ *
+ * @author Macc Liu <mail@maccliu.com>
+ */
+class ContainerException extends \Exception implements ContainerException
+{
+
+}
---
+++ src/Service/NotFoundException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Soil: An underlying PHP framework, under the MIT license.
+ *
+ * Copyright (c) 2016-2017, Macc Liu <mail@maccliu.com>
+ * WITHOUT WARRANTY OF ANY KIND
+ */
+
+namespace Soil\Service;
+
+use Interop\Container\Exception\NotFoundException;
+
+/**
+ * NotFoundException
+ *
+ * @author Macc Liu <mail@maccliu.com>
+ */
+class NotFoundException extends \Exception implements NotFoundException
+{
+
+}
