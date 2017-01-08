0ebfbae9333fe1ca256dafd3726acdd2dd7ce19a
Macc Liu <maccliu@qq.com>
2017-01-08 15:03:08 +0800

    (add) class \Soil\MVC\Model (add) class \Soil\MVC\View

---
+++ src/MVC/View.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Soil: An underlying PHP framework, under the MIT license.
+ *
+ * Copyright (c) 2016-2017, Macc Liu <mail@maccliu.com>
+ * WITHOUT WARRANTY OF ANY KIND
+ */
+
+namespace Soil\MVC;
+
+use \Soil\Application;
+use \Soil\Controller;
+
+/**
+ * View
+ *
+ * @author Macc Liu <mail@maccliu.com>
+ */
+class View
+{
+}
